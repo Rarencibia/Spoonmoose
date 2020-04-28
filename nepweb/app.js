b37a2d26ce9457bd1b17e4b2549571c22682b44f
Myles <myles.leadbeater@gmail.com>
2020-04-29 03:30:08 +0930

    grouped routing lines together

--- nepweb/app.js
+++ nepweb/app.js
@@ -5,12 +5,6 @@ var cookieParser = require('cookie-parser');
 // var lessMiddleware = require('less-middleware');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var about_router = require('./routes/about');
-var contact_router = require('./routes/contact');
-var products_router = require('./routes/products');
-var blog_router = require('./routes/blog')
-var tShirts_router = require('./routes/tShirts')
 //Routes is similiar to "include."
 
 var app = express();
@@ -25,11 +19,25 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+
+var indexRouter = require('./routes/index');
 app.use('/', indexRouter);
+
+var about_router = require('./routes/about');
 app.use('/about', about_router);
+
+var contact_router = require('./routes/contact');
 app.use('/contact', contact_router);
+
+var products_router = require('./routes/products');
 app.use('/products', products_router);
+
+var blog_router = require('./routes/blog')
 app.use('/blog', blog_router);
+
+var tShirts_router = require('./routes/tShirts')
+app.use('/tShirts', tShirts_router);
+
 app.use('/tShirts', tShirts_router);
 
 
